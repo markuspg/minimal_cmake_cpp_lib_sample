c9f25648ca8099b1482ea761fcc48c9654ffba78
Markus Prasser <markus.prasser@zuehlke.com>
2020-03-16 16:26:40 +0100

    Create minimal library sample

---
+++ src/multiply.cpp
@@ -0,0 +1,5 @@
+#include "multiply.h"
+
+uint32_t multiply(uint32_t multiplicant, uint32_t multiplier) {
+    return multiplicant * multiplier;
+}
---
+++ src/multiply.h
@@ -0,0 +1,3 @@
+#include <cstdint>
+
+uint32_t multiply(uint32_t multiplicant, uint32_t multiplier);
